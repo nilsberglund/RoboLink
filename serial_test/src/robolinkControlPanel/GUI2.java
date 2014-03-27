0df71c91ebd882de60f15970b6417868242fdb62
tobias-ahlstrom <ahlstrom.tobias@gmail.com>
2014-03-27 08:40:55 +0100

    Skapat nytt GUI2
    Skapat nytt GUI2

---
+++ serial_test/src/robolinkControlPanel/GUI2.java
@@ -0,0 +1,19 @@
+package robolinkControlPanel;
+
+public class GUI2 extends javax.swing.JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public GUI2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
