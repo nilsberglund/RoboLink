a6e4b6e4e574f8d2dd58e491d44c37f4b6582a15
tobias-ahlstrom <ahlstrom.tobias@gmail.com>
2014-03-27 09:28:19 +0100

    Skapar kopior av alla klasser
    Skapar kopior av alla klasser för att börja om från scratch

--- serial_test/src/robolinkControlPanel/GUI2.java
+++ serial_test/src/robolinkControlPanel/GUI2.java
@@ -1,14 +1,34 @@
 package robolinkControlPanel;
 
 public class GUI2 extends javax.swing.JFrame {
-
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	//Communicator object
+    Communicator2 communicator = null;
+    //KeybindingController object
+    KeybindingController2 keybindingController = null;
 
-	public GUI2() {
-		// TODO Auto-generated constructor stub
+    /** Creates new form GUI */
+    public GUI2() {
+        initComponents();
+        createObjects();
+        communicator.searchForPorts();
+        keybindingController.toggleControls();
+        keybindingController.bindKeys();
+    }
+
+	private void createObjects() {
+		communicator = new Communicator2(this);
+        keybindingController = new KeybindingController2(this);
+		
+		
+	}
+
+	private void initComponents() {
+		
+		
 	}
 
 	public static void main(String[] args) {
