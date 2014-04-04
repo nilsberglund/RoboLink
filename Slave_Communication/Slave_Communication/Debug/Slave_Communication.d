193588e49b8216046632b3e8b7192ed2b1fa62f1
victoralexiev <victor.alexiev@gmail.com>
2014-04-04 18:14:22 +0200

    SPI - Rätt kod
    Master och slave uppkopplade och fungerar med transmit.

--- Slave_Communication/Slave_Communication/Debug/Slave_Communication.d
+++ Slave_Communication/Slave_Communication/Debug/Slave_Communication.d
@@ -10,9 +10,7 @@ Slave_Communication.d Slave_Communication.o: .././Slave_Communication.c \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/version.h \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/fuse.h \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/lock.h \
- c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/util/delay.h \
- c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/util/delay_basic.h \
- c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/math.h
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/interrupt.h
 
 c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/io.h:
 
@@ -36,8 +34,4 @@ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8
 
 c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/lock.h:
 
-c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/util/delay.h:
-
-c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/util/delay_basic.h:
-
-c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/math.h:
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.2.1002\avr8-gnu-toolchain\bin\../lib/gcc/avr/4.7.2/../../../../avr/include/avr/interrupt.h:
--- Slave_Communication/Slave_Communication/Slave_Communication.c
+++ Slave_Communication/Slave_Communication/Slave_Communication.c
@@ -5,60 +5,44 @@
  *  Author: albal214
  */ 
 
-
-#define F_CPU 1000000UL
 #include <avr/io.h>
-#include <util/delay.h>
-
-#define SS PORTB4
-#define MOSI PORTB5
-#define MISO PORTB6
-#define SCK PORTB7
-#define INT_Req PORTB3
+#include <avr/interrupt.h>
 
 uint8_t received_data;
 
 void SPI_Init_Slave();
-void Slave_TX();
-void Slave_RX();
+void Slave_TX(uint8_t);
+
+ISR(SPI_STC_vect)
+{
+	received_data = SPDR;
+	PORTD = SPDR;
+}
+
 
 int main(void)
 {
+	SPI_Init_Slave();
     while(1)
     {
-		
+		Slave_TX(0xAA);
     }
-	
-	void SPI_Init_Slave()
+
+}
+
+void SPI_Init_Slave()
 	{
-		/* PORTB5 MOSI, PORTB6 MISO, PORTB7 SCK, PORTB4 SS, Sets I/O ports */
-		//DDRB |= (0 << PORTB5)|(1 << PORTB6)|(0 << PORTB7)|(0 << PORTB4); 
-		DDRB |= (0<<MOSI)|(1<<MISO)|(0<<SCK)|(0<<SS);
-		
-		DDRB |= (1<<INT_Req); // Sets PortB3 as output. PORTB3 is used to initiate interrupt.
-		
-		/*Sets the SPI Control Register. SPE = SPI Enable. MSTR = Master Select. Master if 1 << MSTR. SPIE = SPI Interrupt Enable. CPOL = Clock Polarity. If 1 << CPOL SCK is High when idle. CPHA = Clock Phase. 
-		0 << CPHA => Leading edge sample , trailing edge Setup. SPR0 = SPI clock rate select. 1 << SPR0, 0 << SPR1 fck/16.*/
-		SPCR = (1<<SPE)|(0<<MSTR)|(1<<SPR0)|(1<<SPR1)|(1<<SPIE)|(1 << CPOL) | (0 << CPHA);
+			DDRD = 0xFF;
+			DDRB = 0x48;
+			
+			SPCR = 0xC0;
+			
+			sei(); 
 	}
 	
-	void Slave_RX()
-	{
-			/* Wait for reception complete */
-			while(!(SPSR & (1<<SPIF)))
-			;
-			/* Return Data Register */
-			received_data = SPDR;
-	}
-	
-	void Slave_TX(uint8_t data)
+void Slave_TX(uint8_t data)
 	{
 		SPDR = data;
-		PORTB = (1 << INT_Req); // Sets INT_Req high. I.e throws interrupt.
-		_delay_ms(10);
-		PORTB = (0 << INT_Req); // Sets INT_Req low. Interrupt finished.
-		
-		/*Wait for transmission complete*/
-		Slave_RX();
+		PORTB |= (1 << PORTB3); // Sets INT_Req high. I.e throws interrupt.
+		PORTB &= ~(1 << PORTB3); // Sets INT_Req low. Interrupt finished.
 	}
-}
