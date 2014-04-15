d99d8bef7b4bac773de5c77011756dec0678f8f4
tobias-ahlstrom <ahlstrom.tobias@gmail.com>
2014-04-15 12:51:35 +0200

    lägger till projekt för bluethooth reciver

---
+++ Bluetooth Receiver/Bluetooth Receiver/Bluetooth Receiver.c
@@ -0,0 +1,17 @@
+/*
+ * Bluetooth_Receiver.c
+ *
+ * Created: 15/04/2014 12:49:29
+ *  Author: Tobias
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
