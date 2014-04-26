17b4d0295725066ae4097e47878ee341db9f1892
tobias-ahlstrom <ahlstrom.tobias@gmail.com>
2014-04-26 16:33:04 +0200

    GUI

--- RobolinkControl/src/robolinkControlPanel/GUI2.java
+++ RobolinkControl/src/robolinkControlPanel/GUI2.java
@@ -11,6 +11,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+
 import javax.swing.JTextArea;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
